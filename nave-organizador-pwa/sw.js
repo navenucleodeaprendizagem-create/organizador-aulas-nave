6fcd83c1a5207e7a31e7ae80db0613a5fccdebc2
navenucleodeaprendizagem-create <nave.nucleodeaprendizagem@gmail.com>
2026-02-08 11:13:55 -0300

    Add files via upload

---
+++ nave-organizador-pwa/sw.js
@@ -0,0 +1,45 @@
+/* NAVE Organizador PWA - service worker */
+const CACHE = "nave-organizador-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        // Cache successful same-origin responses
+        const url = new URL(req.url);
+        if (url.origin === self.location.origin && res.status === 200) {
+          const copy = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(req, copy));
+        }
+        return res;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
